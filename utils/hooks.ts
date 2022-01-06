79cac66ba08c6ef48f1c5d1f76aca5554cb31d92
Vilos92 <linscheid.greg@gmail.com>
2022-01-06 00:14:40 -0800

    :wrench: Further application of useTimeout hook.

--- utils/hooks.ts
+++ utils/hooks.ts
@@ -4,32 +4,29 @@ import {useCallback, useEffect, useRef, useState} from 'react';
  * Hooks
  */
 
-export function useDebounce(value: string, delay: number) {
-  const [debouncedValue, setDebouncedValue] = useState(value);
-
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
-
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [value, delay]);
-
-  return debouncedValue;
-}
-
 export function useTimeout() {
   const timeoutRef = useRef<number>();
-  useEffect(() => () => clearTimeout(timeoutRef.current), []);
+  useEffect(() => () => window.clearTimeout(timeoutRef.current), []);
 
   const setTimeoutCallback = useCallback((handler: () => void, timeout?: number) => {
-    clearTimeout(timeoutRef.current);
+    window.clearTimeout(timeoutRef.current);
     timeoutRef.current = window.setTimeout(handler, timeout);
   }, []);
 
-  const clearTimeoutCallback = useCallback(() => clearTimeout(timeoutRef.current), []);
+  const clearTimeoutCallback = useCallback(() => window.clearTimeout(timeoutRef.current), []);
 
   return [setTimeoutCallback, clearTimeoutCallback];
+}
+
+export function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  const [setTimeout] = useTimeout();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+  }, [value, delay]);
+
+  return debouncedValue;
 }
