114d4af77cda4310ba94555c4da198cc0f88aa96
Vilos92 <linscheid.greg@gmail.com>
2022-01-26 01:34:29 -0800

    :wrench: Custom google login button.

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -1,6 +1,9 @@
+import {ClientSafeProvider} from 'next-auth/react';
+import Image from 'next/image';
 import {FC} from 'react';
 import tw, {styled} from 'twin.macro';
 
+import googleSvg from '../public/google.svg';
 import {Handler} from '../types/common';
 import {Providers, signInWithProvider} from '../utils/auth';
 import {Button} from './Button';
@@ -17,8 +20,7 @@ interface LoginModalProps {
 }
 
 interface LoginButtonProps {
-  providerId: string;
-  providerName: string;
+  provider: ClientSafeProvider;
 }
 
 /*
@@ -39,6 +41,7 @@ const StyledLoginButton = styled(Button)`
   ${tw`
     bg-purple-900
     w-full
+    gap-1
   `}
 `;
 
@@ -46,19 +49,23 @@ const StyledLoginButton = styled(Button)`
  * Components.
  */
 
-export const LoginModal: FC<LoginModalProps> = ({providers, closeModal}) => (
-  <Modal closeModal={closeModal}>
-    <StyledCard>
-      <StyledCardH1>Create your account</StyledCardH1>
-      {Object.values(providers).map(provider => (
-        <LoginButton key={provider.id} providerId={provider.id} providerName={provider.name} />
-      ))}
-    </StyledCard>
-  </Modal>
-);
+export const LoginModal: FC<LoginModalProps> = ({providers, closeModal}) => {
+  const googleProvider = Object.values(providers).find(provider => provider.id === 'google');
+  if (!googleProvider) return null;
 
-const LoginButton: FC<LoginButtonProps> = ({providerId, providerName}) => (
-  <StyledLoginButton onClick={() => signInWithProvider(providerId)}>
-    Sign in with {providerName}
+  return (
+    <Modal closeModal={closeModal}>
+      <StyledCard>
+        <StyledCardH1>Create your account</StyledCardH1>
+        <GoogleLoginButton key={googleProvider.id} provider={googleProvider} />
+      </StyledCard>
+    </Modal>
+  );
+};
+
+const GoogleLoginButton: FC<LoginButtonProps> = ({provider}) => (
+  <StyledLoginButton onClick={() => signInWithProvider(provider.id)}>
+    <Image width={24} src={googleSvg} />
+    Sign in with {provider.name}
   </StyledLoginButton>
 );
