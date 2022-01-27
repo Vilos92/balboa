9de061647d538ec77289bc8b8f8b2c1b68195210
Vilos92 <linscheid.greg@gmail.com>
2022-01-26 23:01:05 -0800

    :wrench: Add nprogress.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import {SessionProvider} from 'next-auth/react';
 import {AppProps} from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 import {FC} from 'react';
 
 import GlobalStyles from './../components/GlobalStyles';
 
 const App: FC<AppProps> = ({Component, pageProps}) => (
   <SessionProvider session={pageProps.session}>
+    <NextNProgress />
     <GlobalStyles />
     <Component {...pageProps} />
   </SessionProvider>
