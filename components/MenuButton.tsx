3cad4b2e7c63eac8b5f35657c4cd49167ccaf965
Vilos92 <linscheid.greg@gmail.com>
2022-03-23 22:44:23 -0700

    :wrench: Add hamburger modal and restyle menu generally.

--- components/MenuButton.tsx
+++ components/MenuButton.tsx
@@ -8,7 +8,9 @@ import {Providers, useAuthSession} from '../utils/auth';
 import {useClickWindow} from '../utils/hooks';
 import {ChromelessButton} from './ChromelessButton';
 import {LoginModal} from './LoginModal';
+import {Modal} from './Modal';
 import {Popover} from './popovers/Popover';
+import {GrueSvg} from './svg/GrueSvg';
 
 /*
  * Props
@@ -55,9 +57,14 @@ const StyledHamburgerPattyDiv = tw.div`
 `;
 
 const StyledMenuCard = tw.div`
+  w-40
+  sm:w-auto
+
   flex
   flex-col
-  bg-white
+
+  bg-gray-200
+
   text-black
   shadow-md
   rounded-2xl
@@ -72,28 +79,47 @@ const StyledMenuItemDiv = tw.div`
   last:pb-3
 
   not-last:border-b-2
-  border-gray-200
+  border-gray-300
 
-  pt-1
-  pb-1
-  pl-3
-  pr-3
+  pt-2
+  pb-2
+  sm:pt-1
+  sm:pb-1
+
+  sm:pl-3
+  sm:pr-3
 
   w-full
   h-full
   text-center
-  hover:bg-gray-100
+
+  hover:bg-gray-300
 `;
 
 const StyledMenuItemButton = tw(ChromelessButton)`
   w-full
   h-full
 
-  text-xl
-  text-gray-500
+  text-3xl
+  sm:text-xl
+  text-gray-600
   active:text-purple-400
   focus:text-purple-400
   hover:text-purple-400
+`;
+
+const StyledModalDiv = tw.div`
+  sm:invisible
+`;
+
+const StyledSvgDiv = tw.div`
+  w-full
+  pt-2
+  pb-1
+
+  flex
+  flex-row
+  justify-center
 `;
 
 /*
@@ -127,7 +153,7 @@ export const MenuButton: FC<MenuButtonProps> = ({providers}) => {
       <Popover
         placement='bottom-end'
         isVisible={isMenuVisible}
-        popoverChildren={<Menu openLoginModal={openLoginModal} closeMenu={closeMenu} />}
+        popoverChildren={<PopoverMenu openLoginModal={openLoginModal} closeMenu={closeMenu} />}
       >
         <StyledMenuButton onClick={openMenu}>
           <StyledHamburgerDiv>
@@ -137,12 +163,13 @@ export const MenuButton: FC<MenuButtonProps> = ({providers}) => {
           </StyledHamburgerDiv>
         </StyledMenuButton>
       </Popover>
+      {isMenuVisible && <ModalMenu openLoginModal={openLoginModal} closeMenu={() => undefined} />}
       {isLoginModalVisible && providers && <LoginModal providers={providers} closeModal={closeLoginModal} />}
     </StyledMenuWrapperDiv>
   );
 };
 
-const Menu: FC<MenuProps> = ({openLoginModal, closeMenu}) => {
+const PopoverMenu: FC<MenuProps> = ({openLoginModal, closeMenu}) => {
   const router = useRouter();
   const {isAuthenticated} = useAuthSession();
 
@@ -163,6 +190,35 @@ const Menu: FC<MenuProps> = ({openLoginModal, closeMenu}) => {
       </StyledMenuItemDiv>
       {menuItems}
     </StyledMenuCard>
+  );
+};
+
+const ModalMenu: FC<MenuProps> = ({openLoginModal, closeMenu}) => {
+  const router = useRouter();
+  const {isAuthenticated} = useAuthSession();
+
+  const onClickCreate = () => router.push('/');
+  const onClickPlans = () => router.push('/plans/');
+  const onClickLogout = () => signOut();
+
+  const menuItems = isAuthenticated
+    ? renderAuthenticatedRoutes(onClickPlans, onClickLogout)
+    : renderUnauthenticatedRoutes(openLoginModal);
+
+  return (
+    <StyledModalDiv>
+      <Modal closeModal={closeMenu}>
+        <StyledMenuCard>
+          <StyledSvgDiv>
+            <GrueSvg fill='#6b7280' height='32px' />
+          </StyledSvgDiv>
+          <StyledMenuItemDiv>
+            <StyledMenuItemButton onClick={onClickCreate}>Create</StyledMenuItemButton>
+          </StyledMenuItemDiv>
+          {menuItems}
+        </StyledMenuCard>
+      </Modal>
+    </StyledModalDiv>
   );
 };
 
