12c5a3ca48f25d3d6f87008bfd270209aed13ff4
Vilos92 <linscheid.greg@gmail.com>
2022-06-19 19:59:52 -0700

    :wrench: Add hover tooltip.

---
+++ stories/components/HoverTooltip.stories.tsx
@@ -0,0 +1,52 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import React from 'react';
+import tw from 'twin.macro';
+
+import {ColumnJustified} from '../../components/Commons';
+import {HoverTooltip as HoverTooltipComponent} from '../../components/popover/HoverTooltip';
+
+/*
+ * Types.
+ */
+
+type TooltipType = typeof HoverTooltipComponent;
+
+/*
+ * Styles.
+ */
+
+const StyledAnchorDiv = tw.div`
+  flex
+  items-center
+
+  bg-gray-300
+  p-3
+  rounded-lg
+`;
+
+/*
+ * Config.
+ */
+
+export default {
+  title: 'Components/Popover',
+  component: HoverTooltipComponent,
+  args: {
+    placement: 'right',
+    text: 'Hover Tooltip!'
+  }
+} as ComponentMeta<TooltipType>;
+
+const Template: ComponentStory<TooltipType> = args => (
+  <ColumnJustified>
+    <HoverTooltipComponent {...args}>
+      <StyledAnchorDiv>Anchor</StyledAnchorDiv>
+    </HoverTooltipComponent>
+  </ColumnJustified>
+);
+
+/*
+ * Stories.
+ */
+
+export const HoverTooltip = Template.bind({});
