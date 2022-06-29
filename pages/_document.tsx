e52c1ec32ba85a14431a512003f1c33cc395293a
Vilos92 <linscheid.greg@gmail.com>
2022-06-28 23:40:11 -0700

    :wrench: Fix types for styles.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
-import Document, {DocumentContext, Head, Html, Main, NextScript} from 'next/document';
+import Document, {DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
     try {
@@ -14,12 +14,12 @@ export default class MyDocument extends Document {
 
       return {
         ...initialProps,
-        styles: (
+        styles: [
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        )
+        ]
       };
     } finally {
       sheet.seal();
